61dba8a14fee8b06f0849c955fca74d8bedde34c
Sam Joffe <slj222@lehigh.edu>
2020-03-28 23:10:20 -0700

    Add some buttons and listeners to comments

--- android/app/src/main/java/edu/lehigh/cse216/slj222/CommentAdapter.java
+++ android/app/src/main/java/edu/lehigh/cse216/slj222/CommentAdapter.java
@@ -1,9 +1,12 @@
 package edu.lehigh.cse216.slj222;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -26,12 +29,14 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
         TextView cText;
         ImageButton profilePic;
         TextView postedBy;
+        Button editButton;
 
         public CommentViewHolder(View itemView) {
             super(itemView);
             cText = itemView.findViewById(R.id.commentText);
             profilePic = itemView.findViewById(R.id.profilePic);
             postedBy = itemView.findViewById(R.id.posted_by);
+            editButton = itemView.findViewById(R.id.editButton);
         }
     }
 
@@ -48,6 +53,27 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
         final Comment d = myData.get(position);
         holder.cText.setText(d.userId); // TODO: Fix this once Vinny fixes that
         holder.profilePic.setBackgroundResource(R.drawable.blank_profile); // Replace later with Google pic
+
+        SharedPreferences sharedPref = context.getSharedPreferences("Shared", Context.MODE_PRIVATE);
+        String userId = sharedPref.getString("userId", "0");
+
+
+        if (d.comment.equals(userId)) { // TODO: Change this when Vinny fixes
+            holder.editButton.setVisibility(View.VISIBLE);
+        } else {
+            holder.editButton.setVisibility(View.INVISIBLE);
+        }
+
+        holder.editButton.setOnClickListener(b -> {
+            // TODO: Create an editing activity, take user there, then HTTP put
+        });
+
+        holder.profilePic.setOnClickListener(b -> {
+            Intent intent = new Intent(b.getContext(), Profile.class);
+            // Add extra for profiledID + logged in ID
+            intent.putExtra("profiledUser", d.comment); // TODO: Change when Vinny fixes
+            context.startActivity(intent);
+        });
     }
 
     @Override
