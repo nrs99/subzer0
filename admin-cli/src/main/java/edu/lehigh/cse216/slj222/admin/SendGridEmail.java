9c754d7417116e525b33563391cac5563e05f764
Sam Joffe <slj222@lehigh.edu>
2020-04-26 10:27:56 -0700

    Created SendGridEmail class

---
+++ admin-cli/src/main/java/edu/lehigh/cse216/slj222/admin/SendGridEmail.java
@@ -0,0 +1,35 @@
+package edu.lehigh.cse216.slj222.admin;
+
+import com.sendgrid.*;
+import java.io.IOException;
+
+public class SendGridEmail {
+
+    private static final SendGrid sg = new SendGrid(
+            "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");
+
+    private SendGridEmail() {
+    } // Don't have constructor
+
+    public static sendEmail(String email, String subject, String myContent) {
+        Email from = new Email("subzer0.cse216@gmail.com");
+        Email to = new Email(email);
+        Content content = new Content("text/plain", "myContent");
+        Mail mail = new Mail(from, subject, to, content);
+
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            System.out.println(response.getStatusCode());
+            System.out.println(response.getBody());
+            System.out.println(response.getHeaders());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+}
