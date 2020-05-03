0c48592ac0b79132c366e059aea85ef9e3194cd6
Sam Joffe <slj222@lehigh.edu>
2020-05-02 18:14:07 -0700

    Following class complete

--- admin-cli/src/main/java/edu/lehigh/cse216/slj222/admin/Following.java
+++ admin-cli/src/main/java/edu/lehigh/cse216/slj222/admin/Following.java
@@ -18,13 +18,13 @@ public class Following {
     public Following(Database db) {
         this.db = db;
         try {
-        createTable = db.Connection.prepareStatement(
-                "CREATE TABLE if not exists following(usera varchar(30), userb varchar(30), FOREIGN KEY (usera) references users (userid), FOREIGN KEY (userb) references users (userid)) ");
-        selectAll = db.Connection.prepareStatement("select * from following");
-        selectFollowers = db.Connection.prepareStatement("select usera from following where userb = ?");
-        selectFollowing = db.Connection.prepareStatement("select userb from following where usera = ?");
-        newFollow = db.Connection.prepareStatement("insert into following values(?, ?)");
-        dropTable = db.Connection.prepareStatement("drop table following");
+            createTable = db.Connection.prepareStatement(
+                    "CREATE TABLE if not exists following(usera varchar(30), userb varchar(30), FOREIGN KEY (usera) references users (userid), FOREIGN KEY (userb) references users (userid)) ");
+            selectAll = db.Connection.prepareStatement("select * from following");
+            selectFollowers = db.Connection.prepareStatement("select usera from following where userb = ?");
+            selectFollowing = db.Connection.prepareStatement("select userb from following where usera = ?");
+            newFollow = db.Connection.prepareStatement("insert into following values(?, ?)");
+            dropTable = db.Connection.prepareStatement("drop table following");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -46,11 +46,73 @@ public class Following {
             System.out.println("  -------------------------");
 
             while (rs.next()) {
-                System.out.printf("%-30s %-30s\n", rs.getString(0), rs.getString(1));
+                System.out.printf("%-30s %-30s\n", rs.getString(1), rs.getString(2));
             }
         } catch (SQLException e) {
-
+            e.printStackTrace();
         }
+    }
+
+    public void selectFollowers(String user) {
+        try {
+            selectFollowers.setString(1, user);
+            ResultSet rs = selectFollowers.executeQuery();
+            System.out.printf("  Current Followers of %s\n", user);
+            System.out.println("  -------------------------");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void selectFollowing(String user) {
+        try {
+            selectFollowing.setString(1, user);
+            ResultSet rs = selectFollowing.executeQuery();
+            System.out.printf("  Current Users %s is Following\n", user);
+            System.out.println("  -------------------------");
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void newFollow(String usera, String userb) { // TODO
+        try {
+            newFollow.setString(1, usera);
+            newFollow.setString(2, userb);
+            ResultSet rs = selectAll.executeQuery();
+            System.out.println("Row added");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void dropTable() {
+        try {
+            createTable.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String options() {
+        return "C*SG+D?";
+    }
+
+    public void help() {
+        System.out.println("Following Menu");
+        System.out.println("  [C] Create following table");
+        System.out.println("  [*] Select all in following table");
+        System.out.println("  [S] View all followers of a particular user");
+        System.out.println("  [G] View all users a particular user is following");
+        System.out.println("  [+] Add a new follow");
+        System.out.println("  [D] Drop following table");
+        System.out.println("  [?] Help - this menu");
     }
 
 }
